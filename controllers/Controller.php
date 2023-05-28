688e44375627840c9a372f7b86e65b1cfd2d2f63
Margarida2223355 <2223355@my.ipleiria.pt>
2023-05-28 19:11:10 +0100

    Classe Controller.

---
+++ controllers/Controller.php
@@ -0,0 +1,51 @@
+<?php
+    //Criar a classe Controller
+    class Controller {
+        /* Definir método de redirecionar para rota.
+         * Este método recebe o prefixo do controlador, a ação e os parâmetros.
+        */
+        protected function redirectToRoute($controllerPrefix, $action, $params = []) {
+
+            //URL através do prefixo e da ação
+            $url = 'index.php?c='
+                . urldecode($controllerPrefix)
+                . "&a="
+                . urldecode($action);
+
+            //Caso existam parâmetros e enviar, enviar através do URL
+            foreach($params as $key => $value) {
+                $url .= '&'
+                    . urldecode($key)
+                    . '='
+                    . urldecode($value);
+            }
+
+            //Redirecionar utilizando o URL final
+            header('Location: ' . $url);
+        }
+
+        /* Definir método para mostrar a view pretendida.
+         * Este método recebe o prefiro do controlador, o nome da view, dados (podem não ser enviados)
+         * e por último o layout da página (default).
+        */
+        protected function renderView($controllerPrefix, $viewName, $data = [], $layout = 'default') {
+            //Converter o array associativo num conjunto de variáveis visíveis
+            extract($data);
+
+            //Definir variável ViewPath que mostrará a vista pretendida
+            $viewPath = 'views/'
+                . $controllerPrefix
+                . '/'
+                . $viewName
+                . '.php';
+
+            //Definir variável layoutPath para mostrar a vista layout
+            $layoutPath = 'views/layout'
+                . $layout
+                . '.php';
+
+            //Incluir ficheiro do layout
+            require_once ($layoutPath);
+        }
+    }
+?>
--- views/layout/default.php
+++ views/layout/default.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-
-    <title><?= APP_NAME ?></title>
-</head>
-<body>
-    <?php
-        require_once $viewPath;
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-</body>
+<html>
+   <head>
+      <title><?= APP_NAME?></title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   </head>
+   <body>
+      <?php
+         //Aqui já teremos a variável $viewPath definida no Controller - linha 36
+         require_once($viewPath);
+      ?>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   </body>
 </html>
