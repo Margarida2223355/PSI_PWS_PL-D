58d349c964028bda44aabf88e759c2ecb77207c3
Margarida2223355 <2223355@my.ipleiria.pt>
2023-05-28 15:33:03 +0100

    Ligação base de dados.

--- startup/config.php
+++ startup/config.php
@@ -3,4 +3,14 @@
 
     define('APP_NAME', 'Folha de Obra');
     define('INVALID_ACCESS_ROUTE', 'c=auth&a=index');
+
+    //Ligação à base de dados
+    ActiveRecord\Config::initialize(function($cfg) {
+        $cfg -> set_model_directory('./modelos');
+        $cfg -> set_connections(
+            array (
+                'development' => 'mysql://root@localhost/projeto',
+            )
+            );
+    });
 ?>
