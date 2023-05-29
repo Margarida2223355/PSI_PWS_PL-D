6c67dbd3f5c32dcfad9c5f71391558ebc2d7537d
Margarida2223355 <2223355@my.ipleiria.pt>
2023-05-29 21:09:08 +0100

    Layout vista Auth.

---
+++ views/auth/index.php
@@ -0,0 +1,23 @@
+<div class="card-header">
+    <h3 class="card-title">Formulário Login</h3>
+</div>
+<form class="form-horizontal">
+    <div class="card-body">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Username:</label>
+            <div class="caol-sm-10">
+                <input type="text" class="form-control" id="fuser">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Password:</label>
+            <div class="caol-sm-10">
+                <input type="password" class="form-control" id="fpass">
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <input type="submit" class="btn btn-info" value="Log in">
+        <input type="submit" class="btn btn-default float-right" value="Cancel">
+    </div>
+</form>
--- views/layout/default.php
+++ views/layout/default.php
@@ -5,14 +5,6 @@
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
    </head>
    <body>
-      <?php
-         //Aqui já teremos a variável $viewPath definida no Controller - linha 36
-         require_once($viewPath);
-      ?>
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
       <nav class="main-header navbar navbar-expand navbar-light" style="background-color: darkcyan;">
          <ul class="navbar-nav">
             <li class="nav-item">
@@ -28,5 +20,13 @@
             </li>
          </ul>
       </nav>
+      <?php
+         //Aqui já teremos a variável $viewPath definida no Controller - linha 36
+         require_once($viewPath);
+      ?>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
    </body>
 </html>
